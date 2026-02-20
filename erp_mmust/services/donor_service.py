cbe5d5c1810e00e8ee53a5c5be2a7350eb8939d9
Ajani Timothy <timothyinioluwa@gmail.com>
2026-02-20 16:16:05 +0100

    fix:added parent acount for general ledger to mmust donor settings

--- erp_mmust/services/donor_service.py
+++ erp_mmust/services/donor_service.py
@@ -6,13 +6,12 @@ import random
 def create_donor_gl_account(doc, method=None):
     company = get_company()
 
-    parent_account = frappe.db.get_value(
-        "Account",
-        {"account_name": "Refundable Deposit", "company": company, "is_group": 1},
-        "name"
-    )
+    parent_account = frappe.db.get_single_value("MMUST Donor Settings", "default_sponsor_parent_account")
     if not parent_account:
-        frappe.throw("Refundable Deposit group account not found in Chart of Accounts.")
+        frappe.throw(
+            "No Default Sponsor Parent Account set in <b>MMUST Donor Settings</b>. "
+            "Please set it before creating donors."
+        )
 
     account = frappe.new_doc("Account")
     account.account_name = f"{doc.donor_name} Sponsor"
